00a47744d0ee6149aed906e106620962d6e3a8bf
Felipe Andrade <felipes.andrade@gmail.com>
2021-10-20 07:00:43 -0300

    login page

---
+++ lib/modules/login/login_page.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+import 'package:payflow/shared/themes/app_colors.dart';
+import 'package:payflow/shared/themes/app_images.dart';
+import 'package:payflow/shared/themes/app_text_style.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    // Aramazena o tamanho da tela na variável size
+    final size = MediaQuery.of(context).size;
+
+    // Esqueleto da nossa aplicação
+    return Scaffold(
+      // Cor do fundo da nossa aplicação
+      backgroundColor: AppColors.background,
+      //Corpo da nossa tela, utilizando widget de pilha -> stack -> empilhamento de coisas
+      body: Container(
+        width: size.width,
+        height: size.height,
+        child: Stack(
+          // Stack recebe uma lista de children, pois queremos colocar vários intens empilhados
+          children: [
+            // Container responsável por configurar o background do app
+            Container(
+              // Seta o valor do width conforme o valor recebido em size
+              width: size.width,
+              // Seta o valor do height em 30%
+              height: size.height * 0.36,
+              // Seta a cor padrão do container em laranja
+              color: AppColors.primary,
+            ),
+            // Incluindo a imagem e definindo a posição na tela
+            Positioned(
+                top: 40,
+                left: 0,
+                right: 0,
+                // Iserindo imagem e setando o tamanho na tela
+                child: Image.asset(
+                  AppImages.person,
+                  width: 208,
+                  height: 300,
+                )),
+            // Incluindo a imagem menor e o texto
+            Positioned(
+              // Ajusta a posição dinamicamente, conforme o tamanho da tela
+              bottom: size.height * 0.18,
+              left: 0,
+              right: 0,
+              child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    // Adicionando a imagem
+                    Image.asset(AppImages.logomini),
+                    // Adicionando o texto e setando as posição na tela
+                    Padding(
+                      // Posicionamento do texto
+                      padding:
+                          const EdgeInsets.only(top: 30, left: 70, right: 70),
+                      // Texto
+                      child: Text("Organize seus boletos em um só lugar",
+                          // Alinhamento do texto
+                          textAlign: TextAlign.center,
+                          // Estilo do texto
+                          style: AppTextStyles.titleHome),
+                    ),
+                  ]),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/shared/widgets/social_login/social_login_button.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SocialLoginButton extends StatefulWidget {
+  const SocialLoginButton({Key? key}) : super(key: key);
+
+  @override
+  _SocialLoginButtonState createState() => _SocialLoginButtonState();
+}
+
+class _SocialLoginButtonState extends State<SocialLoginButton> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
